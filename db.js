3224713f6694af00a560aba31dd28b3d7180b0f8
gijsmaas82 <gijsmaas82@outlook.com>
2019-12-23 21:01:10 +0100

    Clear heroku db with force

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const databaseUrl = process.env.DATABASE_URL || 'postgres://postgres:secret@loca
 
 const db = new Sequelize(databaseUrl)
 
-db.sync({ force: false })
+db.sync({ force: true })
   .then(console.log('db connected'))
   .catch(console.error)
 
