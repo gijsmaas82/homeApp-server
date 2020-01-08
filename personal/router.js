eaaae5c508b6f6600bbff428d8776da255d26ac7
gijsmaas82 <gijsmaas82@outlook.com>
2020-01-08 16:33:46 +0100

    Delete personal

--- personal/router.js
+++ personal/router.js
@@ -11,8 +11,15 @@ router.post('/personal', (req, res, next) => {
 
 router.get('/personal', (req, res, next) => {
   Personal.findAll()
-  .then(personal => res.json(personal))
-  .catch(err => next(err))
+    .then(personal => res.json(personal))
+    .catch(err => next(err))
+})
+
+router.delete('/personal/:id', (req, res, next) => {
+  Personal.destroy({ where: { id: req.params.id }})
+    .then(res.status(204)
+    .send('Personal removed'))
+    .catch(err => next(err))
 })
 
 module.exports = router
